e8474ccf9f0d078824d8ec30e3b67cffac44eded
SunSeekerX <1647800606@qq.com>
2021-04-28 00:46:10 +0800

    feat: chore

--- src/shared/interceptor/version.interceptor.ts
+++ src/shared/interceptor/version.interceptor.ts
@@ -3,21 +3,21 @@
  * @author: SunSeekerX
  * @Date: 2021-04-26 11:47:39
  * @LastEditors: SunSeekerX
- * @LastEditTime: 2021-04-28 00:31:02
+ * @LastEditTime: 2021-04-28 00:45:54
  */
 
 import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import type { ResponseRO } from 'src/shared/interface/response.interface'
-import { version } from '../../../package.json'
+// import { version } from '../../package.json'
 
 @Injectable()
 export class VersionInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data: ResponseRO) => {
-        data.version = version
+        // data.version = version
         return data
       })
     )
